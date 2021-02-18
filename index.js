f2bd06b03e7a041b0a06675af85d42deab40cf85
fmoreno94 <francisco.moreno@kolonial.no>
2021-02-19 00:16:16 +0100

    Update style

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function randomNumber(min, max) {
 }
 
 function createRouletteSlice(sliceName, position, sliceCount) {
+  const colorNumber = position + 1 - 3 * Math.floor(position / 3);
   const sliceDegrees = 360 / sliceCount;
   const arcRads = degreesToRad(90 - sliceDegrees);
   const arcEndX =
@@ -30,11 +31,15 @@ function createRouletteSlice(sliceName, position, sliceCount) {
 
   const $slice = document.createElementNS("http://www.w3.org/2000/svg", "path");
   $slice.classList.add("slice");
+  $slice.classList.add(`slice-color-${colorNumber}`);
 
   // <path d="M center_x center_y L center_x 1 A arc_size_x arc_size_y 0 0 1 arcEndX arcEndY" />
   $slice.setAttribute(
     "d",
-    `M ${CIRCLE_RADIUS_PLUS_STROKE} ${CIRCLE_RADIUS} L ${CIRCLE_RADIUS_PLUS_STROKE} ${STROKE} A ${CIRCLE_RADIUS_MINUS_STROKE} ${CIRCLE_RADIUS_MINUS_STROKE} 0 0 1 ${arcEndX} ${arcEndY}`
+    `M ${CIRCLE_RADIUS_PLUS_STROKE} ${CIRCLE_RADIUS}
+     L ${CIRCLE_RADIUS_PLUS_STROKE} ${STROKE}
+     A ${CIRCLE_RADIUS_MINUS_STROKE} ${CIRCLE_RADIUS_MINUS_STROKE}
+     0 0 1 ${arcEndX} ${arcEndY}`
   );
   $slice.style.transform = `rotate(${position * sliceDegrees}deg)`;
 
@@ -45,10 +50,7 @@ function createRouletteSlice(sliceName, position, sliceCount) {
   );
   $sliceNamePath.classList.add("slice-name");
 
-  $sliceNamePath.setAttribute(
-    "d",
-    `M ${CIRCLE_RADIUS_PLUS_STROKE} ${CIRCLE_RADIUS} L ${CIRCLE_RADIUS_PLUS_STROKE} ${STROKE} A ${CIRCLE_RADIUS_MINUS_STROKE} ${CIRCLE_RADIUS_MINUS_STROKE} 0 0 1 ${arcEndX} ${arcEndY}`
-  );
+  $sliceNamePath.setAttribute("d", $slice.getAttribute("d"));
   $sliceNamePath.style.transform = `rotate(${
     position * sliceDegrees + sliceDegrees / 2 + 2
   }deg)`;
@@ -78,7 +80,7 @@ function createRoulette(slices) {
     return false;
   }
 
-  const $roulette = $getEle("#roulette");
+  const $roulette = $getEle("#roulette-slices");
   const $rouletteNames = $getEle("#roulette-names");
   $roulette.innerHTML = "";
   $rouletteNames.innerHTML = "";
@@ -105,17 +107,23 @@ $getEle("#generate-roulette").addEventListener("click", () => {
 });
 
 $getEle("#spin").addEventListener("click", () => {
-  const $roulette = $getEle("#roulette-wrapper");
+  const $roulette = $getEle("#roulette");
 
   $roulette.style.transition = "none";
   $roulette.style.transform = `rotate(0deg)`;
 
   setTimeout(() => {
     const completeRounds = randomNumber(6, 12);
-    const animationDuration = randomNumber(3.8, 5);
+    const animationDuration = randomNumber(3.8, 6);
     const finalCoil =
       Math.random() <= 0.7 ? randomNumber(0.95, 1) : randomNumber(1, 1.3);
     $roulette.style.transition = `transform ${animationDuration}s cubic-bezier(0.35,-0.15, 0, ${finalCoil})`;
     $roulette.style.transform = `rotate(${completeRounds * 360}deg)`;
   }, 40);
 });
+
+participants = $getEle("#participants")
+  .value.split("\n")
+  .filter((participant) => participant.trim().length);
+
+const created = createRoulette(participants);
