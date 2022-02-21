92eef9fedee770fdedf43dfb5eed9e333b4d9d34
fmorenodesigns <fmorenodesigns@gmail.com>
2022-02-21 17:23:14 +0100

    Use dashed dot instead of question mark for tooltip

--- src/ui-elements/Instructions/index.tsx
+++ src/ui-elements/Instructions/index.tsx
@@ -6,16 +6,17 @@ export default function Instructions() {
     <div id="instructions">
       <span>
         Separate entries by a comma(,) or a line-break. When spinning the
-        roulette, it randomly
-      </span>
+        roulette, it
+      </span>{" "}
       <Tooltip
         placement="auto-end"
         content="The roulette has been tested against the law of the large numbers:
           with 2 entries, each entry is expected to have a 50% chance of being
           chosen after spinning the wheel a large number of times."
+        className="tooltip-trigger"
       >
-        <i className="far fa-question-circle law-of-large-numbers-explanation"></i>
-      </Tooltip>
+        randomly
+      </Tooltip>{" "}
       <span>picks one of the given entries.</span>
     </div>
   );
