4d995e76b262082c3b2ab13289a6f59f897617ca
Francisco Moreno <francisco.moreno@oda.com>
2024-09-21 19:46:21 +0200

    Attempt improving SEO

--- src/ui-elements/Header/index.tsx
+++ src/ui-elements/Header/index.tsx
@@ -4,7 +4,12 @@ import "./styles.scss";
 export default function Header() {
   return (
     <header className="page-header">
-      <img className="logo" src="./logo.png" alt="logo" height={120} />
+      <img
+        className="logo"
+        src="./logo.png"
+        alt="Spin wheel for decision-making"
+        height={120}
+      />
     </header>
   );
 }
